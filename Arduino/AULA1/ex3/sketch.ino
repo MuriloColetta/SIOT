3d4557737bb39a017e3e5548eb23d6422a2f1d86
Murilo Della Coletta <mucoletta11@gmail.com>
2025-08-19 16:54:12 -0300

    19/08

---
+++ Arduino/AULA1/ex3/sketch.ino
@@ -0,0 +1,102 @@
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+}
+
+void loop() {
+  for(int i = 1; i <= 15; i++) {
+    if (i == 1) {
+      digitalWrite(13, HIGH);
+      digitalWrite(12, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      delay(1500);
+    } else if (i == 2) {
+      digitalWrite(13, LOW);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      delay(1500);
+    } else if (i == 3) {
+      digitalWrite(13, HIGH);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(10, LOW);
+      delay(1500);
+    } else if (i == 4) {
+      digitalWrite(13, LOW);
+      digitalWrite(12, LOW);
+      digitalWrite(11, HIGH);
+      digitalWrite(10, LOW);
+      delay(1500);
+    } else if (i == 5) {
+      digitalWrite(13, HIGH);
+      digitalWrite(12, LOW);
+      digitalWrite(11, HIGH);
+      digitalWrite(10, LOW);
+      delay(1500);
+    } else if (i == 6) {
+      digitalWrite(13, LOW);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(10, LOW);
+      delay(1500);
+    } else if (i == 7) {
+      digitalWrite(13, HIGH);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(10, LOW);
+      delay(1500);
+    } else if (i == 8) {
+      digitalWrite(13, LOW);
+      digitalWrite(12, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      delay(1500);
+    } else if (i == 9) {
+      digitalWrite(13, HIGH);
+      digitalWrite(12, LOW);
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      delay(1500);
+    } else if (i == 10) {
+      digitalWrite(13, LOW);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      delay(1500);
+    } else if (i == 11) {
+      digitalWrite(13, HIGH);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, LOW);
+      digitalWrite(10, HIGH);
+      delay(1500);
+    } else if (i == 12) {
+      digitalWrite(13, LOW);
+      digitalWrite(12, LOW);
+      digitalWrite(11, HIGH);
+      digitalWrite(10, HIGH);
+      delay(1500);
+    } else if (i == 13) {
+      digitalWrite(13, HIGH);
+      digitalWrite(12, LOW);
+      digitalWrite(11, HIGH);
+      digitalWrite(10, HIGH);
+      delay(1500);
+    } else if (i == 14) {
+      digitalWrite(13, LOW);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(10, HIGH);
+      delay(1500);
+    } else {
+      digitalWrite(13, HIGH);
+      digitalWrite(12, HIGH);
+      digitalWrite(11, HIGH);
+      digitalWrite(10, HIGH);
+      delay(1500);
+    }
+  }
+}
